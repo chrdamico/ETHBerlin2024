cb0fb11ed222bd3989713fee24399ef4a2f10327
christian.damico <christian.damico@smart-steel-technologies.com>
2024-05-25 14:13:39 +0200

    primereact import and example button

--- dfp/src/App.jsx
+++ dfp/src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; // theme
+import 'primereact/resources/primereact.min.css'; // core css
+import 'primeicons/primeicons.css'; // icons
+import { Button } from 'primereact/button';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -24,12 +28,13 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <Button label="Example PrimeReact Button" icon="pi pi-check" onClick={() => alert('PrimeReact Button Clicked!')} />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
