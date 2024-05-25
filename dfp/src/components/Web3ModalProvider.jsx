6df36f1e31ad5b318049a56dddac1ae6fee33104
mingo2 <mingo97@outlook.it>
2024-05-26 00:05:23 +0200

    wETH

--- dfp/src/components/Web3ModalProvider.jsx
+++ dfp/src/components/Web3ModalProvider.jsx
@@ -1,43 +1,43 @@
-import { createWeb3Modal } from '@web3modal/wagmi/react'
-import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+import { createWeb3Modal } from "@web3modal/wagmi/react";
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
 
-import { WagmiProvider } from 'wagmi'
-import { hardhat, mainnet } from 'wagmi/chains'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from "wagmi";
+import { hardhat, mainnet, sepolia } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // 0. Setup queryClient
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 // 1. Get projectId at https://cloud.walletconnect.com
-const projectId = 'ETH2024'
+const projectId = "ETH2024";
 
 // 2. Create wagmiConfig
 const metadata = {
-    name: 'Web3Modal',
-    description: 'Web3Modal Example',
-    url: 'https://web3modal.com', // origin must match your domain & subdomain
-    icons: ['https://avatars.githubusercontent.com/u/37784886']
-}
+  name: "Web3Modal",
+  description: "Web3Modal Example",
+  url: "https://web3modal.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
 
-const chains = [hardhat]
+const chains = [hardhat, mainnet, sepolia];
 
 const config = defaultWagmiConfig({
-    chains,
-    projectId,
-    metadata,
-})
+  chains,
+  projectId,
+  metadata,
+});
 
 createWeb3Modal({
-    wagmiConfig: config,
-    projectId,
-    enableAnalytics: true, // Optional - defaults to your Cloud configuration
-    enableOnramp: true // Optional - false as default
-})
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true, // Optional - false as default
+});
 
 export function Web3ModalProvider({ children }) {
-    return (
-        <WagmiProvider config={config}>
-            <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-        </WagmiProvider>
-    )
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
 }
--- dfp/src/hooks/readFnc.jsx
+++ dfp/src/hooks/readFnc.jsx
@@ -2,7 +2,7 @@ import { useReadContract } from "wagmi";
 // import configs from "../configs.json";
 import abiDAI from "../solidity/data/DAI/ABI";
 
-const contractAddress = "0x68194a729C2450ad26072b3D33ADaCbcef39D574";
+const contractAddress = "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9"; //"0x68194a729C2450ad26072b3D33ADaCbcef39D574";
 const fiverrpunkAddress = "0x07E577C64ddA84B130a6F813b1bF73fe51EC503D";
 // configs.isTest
 //   ? configs.address.testnet
