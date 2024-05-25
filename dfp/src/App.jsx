0dc7c9ef8ff6ca9ef984d2bfd75c4249a1c6a6cc
pavel.trounov <pavel.trounov@smart-steel-technologies.com>
2024-05-25 21:26:19 +0200

    Wallet connect

--- dfp/src/App.jsx
+++ dfp/src/App.jsx
@@ -3,15 +3,19 @@ import 'primereact/resources/themes/lara-light-indigo/theme.css'; // theme
 import 'primereact/resources/primereact.min.css'; // core css
 import 'primeicons/primeicons.css'; // icons
 import MyTabs from "./components/MyTabs.jsx";
-import {PrimeReactProvider} from "primereact/api"; // Ensure correct path to MyTabs.jsx
+import {PrimeReactProvider} from "primereact/api";
+import {Web3ModalProvider} from "./components/Web3ModalProvider.jsx"; // Ensure correct path to MyTabs.jsx
 
 function App() {
   return (
+      <Web3ModalProvider>
     <PrimeReactProvider>
         <div>
+            <w3m-button />
             <MyTabs />
         </div>
     </PrimeReactProvider>
+      </Web3ModalProvider>
   );
 }
 
