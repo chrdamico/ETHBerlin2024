c1ec08f3c323f4ce834deea9cdd0a0f2c35fffd3
mingo2 <mingo97@outlook.it>
2024-05-26 00:13:29 +0200

    WETH

--- dfp/src/hooks/readFnc.jsx
+++ dfp/src/hooks/readFnc.jsx
@@ -1,21 +1,23 @@
 import { useReadContract } from "wagmi";
 // import configs from "../configs.json";
 import abiDAI from "../solidity/data/DAI/ABI";
+import abiWETH from "../solidity/data/WETH/ABI";
 
-const contractAddress = "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9"; //"0x68194a729C2450ad26072b3D33ADaCbcef39D574";
+const WETHAddress = "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9";
+const DAIAddress = "0x68194a729C2450ad26072b3D33ADaCbcef39D574";
 const fiverrpunkAddress = "0x07E577C64ddA84B130a6F813b1bF73fe51EC503D";
 // configs.isTest
 //   ? configs.address.testnet
 //   : configs.address.mainnet;
 
-const DAIContract = {
+const WETHContract = {
   address: contractAddress,
-  abi: abiDAI,
+  abi: abiWETH,
 };
 
 export function readAllowance(address) {
   const { data, error, isLoading } = useReadContract({
-    ...DAIContract,
+    ...WETHContract,
     functionName: "allowance",
     args: [address, fiverrpunkAddress],
   });
