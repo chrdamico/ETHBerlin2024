c18b712bb3eb6096b02e4c9fab3d523b59f642cd
pavel.trounov <pavel.trounov@smart-steel-technologies.com>
2024-05-25 15:38:03 +0200

    Migrations and DB

--- dfp_backend/apps/task_board/apps.py
+++ dfp_backend/apps/task_board/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TaskBoardConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'task_board'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.task_board"
